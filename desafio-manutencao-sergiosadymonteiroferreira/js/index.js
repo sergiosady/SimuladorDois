9499572b929eca3565a07aceae76639c375c6184
Sergio Sady <sergiosadymf@gmail.com>
2025-12-10 02:48:42 -0300

    adicionando logo e menu

--- desafio-manutencao-sergiosadymonteiroferreira/js/index.js
+++ desafio-manutencao-sergiosadymonteiroferreira/js/index.js
@@ -2,9 +2,16 @@ function avisar() {
     alert("Você clicou no botão!");
 }
 
+function abrirMenu() {
+    alert("Você clicou no menu!");
+}
+
 function addEvent() {
     const botao = document.getElementById('meuBotao');
     botao.addEventListener('click', avisar);
+
+    const botaoMenu = document.getElementById('menu')
+    botaoMenu.addEventListener('click', abrirMenu);
 }
 
 window.addEventListener('load', addEvent);
