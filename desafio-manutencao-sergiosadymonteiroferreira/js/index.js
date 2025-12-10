29b04dec0f0404c24c000cf463114cf490e750b1
Sergio Sady <sergiosadymf@gmail.com>
2025-12-10 17:03:09 -0300

    botao busca, diminuicao cabecalho

--- desafio-manutencao-sergiosadymonteiroferreira/js/index.js
+++ desafio-manutencao-sergiosadymonteiroferreira/js/index.js
@@ -6,12 +6,21 @@ function abrirMenu() {
     alert("Você clicou no menu!");
 }
 
+function buscarNoSite() {
+    alert("Você clicou na busca!");
+}
+
 function addEvent() {
     const botao = document.getElementById('meuBotao');
     botao.addEventListener('click', avisar);
 
-    const botaoMenu = document.getElementById('menu')
+    const botaoMenu = document.getElementById('botaoMenu');
     botaoMenu.addEventListener('click', abrirMenu);
+
+    const botaoBusca = document.getElementById('botaoBusca');
+    botaoBusca.addEventListener('click', buscarNoSite);
 }
+
+
 
 window.addEventListener('load', addEvent);
