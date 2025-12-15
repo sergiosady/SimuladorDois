dd84045d4f5caa28c5c1d86d3611a7c91a189fd4
Sergio Sady <sergiosadymf@gmail.com>
2025-12-15 12:16:51 -0300

    Substituindo divs

--- desafio-manutencao-sergiosadymonteiroferreira/js/index.js
+++ desafio-manutencao-sergiosadymonteiroferreira/js/index.js
@@ -14,10 +14,10 @@ function addEvent() {
     const botao = document.querySelector('.principal__botao');
     botao.addEventListener('click', avisar);
 
-    const botaoMenu = document.querySelector('.cabecalho__menu');
+    const botaoMenu = document.querySelector('.painel__menu');
     botaoMenu.addEventListener('click', abrirMenu);
 
-    const botaoBusca = document.querySelector('.cabecalho__busca');
+    const botaoBusca = document.querySelector('.painel__busca');
     botaoBusca.addEventListener('click', buscarNoSite);
 }
 
