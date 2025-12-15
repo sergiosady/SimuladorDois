28c8661b042c481b2eee2202d2101e6e0a961033
Sergio Sady <sergiosadymf@gmail.com>
2025-12-15 15:31:58 -0300

    Implementando media query no header

--- desafio-manutencao-sergiosadymonteiroferreira/js/index.js
+++ desafio-manutencao-sergiosadymonteiroferreira/js/index.js
@@ -1,24 +1,24 @@
 function avisar() {
-    alert("Você clicou no botão!");
+  alert("Você clicou no botão!");
 }
 
 function abrirMenu() {
-    alert("Você clicou no menu!");
+  alert("Você clicou no menu!");
 }
 
 function buscarNoSite() {
-    alert("Você clicou na busca!");
+  alert("Você clicou na busca!");
 }
 
 function addEvent() {
-    const botao = document.querySelector('.principal__botao');
-    botao.addEventListener('click', avisar);
+  const botao = document.querySelector(".principal__botao");
+  botao.addEventListener("click", avisar);
 
-    const botaoMenu = document.querySelector('.painel__menu');
-    botaoMenu.addEventListener('click', abrirMenu);
+  const botaoMenu = document.querySelector(".painel__menu");
+  botaoMenu.addEventListener("click", abrirMenu);
 
-    const botaoBusca = document.querySelector('.painel__busca');
-    botaoBusca.addEventListener('click', buscarNoSite);
+  const botaoBusca = document.querySelector(".painel__busca");
+  botaoBusca.addEventListener("click", buscarNoSite);
 }
 
-window.addEventListener('load', addEvent);
+window.addEventListener("load", addEvent);
